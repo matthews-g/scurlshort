38d595aada890f1379b744e83c00629a703fbc18
matthews-g <73955058+matthews-g@users.noreply.github.com>
2022-01-18 18:41:11 +0100

    Tests have been updated
    Now it is working with 'python manage.py test'

--- urlshortener/tests.py
+++ urlshortener/tests.py
@@ -7,43 +7,31 @@ import random
 # Incorrect usage means the unsuccessful queries that were mandatory according to the document
 
 
-class TestBase(APITestCase):
+code_chars = (
+            string.ascii_uppercase
+            + string.ascii_lowercase
+            + string.digits + "_")
 
-    def __init__(self, client: RequestsClient, server_address: str, url_shortcode: dict):
-        self.client = client
-        self.server_address = server_address
-        self.url = ""
-        self.url_shortcode = url_shortcode  # For manual shortcode testing
+test_shortcode = random.sample(code_chars, 6)
 
-    """def test_correct(self):
-        response = self.client.post(self.url, data=self.url_shortcode)  # example
-        # test the optimal usage
-        pass
+CLIENT = RequestsClient()
 
-    def test_incorrect(self):
-        # test incorrect usage
-        pass
-
-    def test_all(self):
-        self.test_correct()
-        self.test_incorrect()"""
+SERVER_ADDRESS = "http://127.0.0.1:8000/"
+URL_SHORTCODE = {"url": "https://www.example.com/", "shortcode": "".join(test_shortcode)}
 
 
-class ShortenTest(TestBase):
-    def __init__(self, client, server_address, url_shortcode):
-        super().__init__(client, server_address, url_shortcode)
-
-        self.url = self.server_address + "shorten/"
+class ShortCodeCreation(APITestCase):
 
     def test_correct(self):
         # Automatic shortcode
-        response = self.client.post(self.url, data={"url": "https://www.example.com/"})
+        response = CLIENT.post(SERVER_ADDRESS+"shorten/", data={"url": "https://www.example.com/"})
         json_resp = json.loads(response.text)
         assert response.status_code == 201, "Response code should be 201"
         assert json_resp['shortcode'] is not None, "Shortcode should be in response"
 
         # Manual shortcode
-        response = self.client.post(self.url, data=self.url_shortcode)
+        response = CLIENT.post(SERVER_ADDRESS+"shorten/", data={"url": "https://www.example.com/",
+                                                                "shortcode": URL_SHORTCODE['shortcode']})
         json_resp = json.loads(response.text)
         assert response.status_code == 201, "Response code should be 201"
         assert json_resp['shortcode'] is not None, "Shortcode should be in response"
@@ -55,44 +43,43 @@ class ShortenTest(TestBase):
         # We only check for status codes because they are explicitly declared with the error msg
         # Only after correct testing!
 
-        response = self.client.post(self.url, data={"url": "https://www.example.com/", "shortcode": "ß$a123"})
+        response = CLIENT.post(SERVER_ADDRESS+"shorten/", data={"url": "https://www.example.com/", "shortcode": "ß!a123"})
         assert response.status_code == 412, "Response code should be 412"
-        response = self.client.post(self.url, data={"url": "", "shortcode": "ß$a123"})
+        response = CLIENT.post(SERVER_ADDRESS+"shorten/", data={"url": "", "shortcode": "ß$a123"})
         assert response.status_code == 400, "Response code should be 400"
-        response = self.client.post(self.url, data=self.url_shortcode)
+
+        response = CLIENT.post(SERVER_ADDRESS + "shorten/", data={"url": "https://www.example.com/",
+                                                                  "shortcode": URL_SHORTCODE['shortcode']})
+        response = CLIENT.post(SERVER_ADDRESS + "shorten/", data={"url": "https://www.example.com/",
+                                                                "shortcode": URL_SHORTCODE['shortcode']})
         assert response.status_code == 409, "Response code should be 409"
 
         print("All shortening related tests passed regarding incorrect cases!")
         return True
 
-
-class ShortCodeTest(TestBase):
-    def __init__(self, client: RequestsClient, server_address: str, url_shortcode: dict):
-        super().__init__(client, server_address, url_shortcode)
-        self.url = self.server_address+url_shortcode['shortcode']+"/"
-        self.incorrect_url = self.server_address+"te$$$/"
+class ShortCodeTest(APITestCase):
 
     def test_correct(self):
-        response = self.client.get(self.url, allow_redirects=False)  # Redirects disabled
+        response = CLIENT.post(SERVER_ADDRESS + "shorten/", data={"url": "https://www.example.com/",
+                                                                  "shortcode": URL_SHORTCODE['shortcode']})
+        response = CLIENT.get(SERVER_ADDRESS + URL_SHORTCODE['shortcode']+"/", allow_redirects=False)
         assert response.status_code == 302, "Response code should be 302"
         print("All shortcode redirect related tests passed regarding correct cases!")
         return True
 
     def test_incorrect(self):
-        response = self.client.get(self.incorrect_url, allow_redirects=False)  # Redirects disabled
+        response = CLIENT.get(SERVER_ADDRESS + 'asdasjdsjd/', allow_redirects=False)  # Redirects disabled
         assert response.status_code == 404, "Response code should be 404"
         print("All shortcode redirect related tests passed regarding incorrect cases!")
         return True
 
 
-class StatTest(TestBase):
-    def __init__(self, client: RequestsClient, server_address: str, url_shortcode: dict):
-        super().__init__(client, server_address, url_shortcode)
-        self.url = self.server_address+url_shortcode['shortcode']+"/stats/"
-        self.incorrect_url = self.server_address + "te$$$/stats/"
+class StatTest(APITestCase):
 
     def test_correct(self):
-        response = self.client.get(self.url)
+        response = CLIENT.post(SERVER_ADDRESS + "shorten/", data={"url": "https://www.example.com/",
+                                                                  "shortcode": URL_SHORTCODE['shortcode']})
+        response = CLIENT.get(SERVER_ADDRESS + URL_SHORTCODE['shortcode'] + "/stats/")
         json_resp = json.loads(response.text)
         assert response.status_code == 200, "Response code should be 200"
         assert json_resp['created'] is not None, "Created should not be none!"
@@ -102,39 +89,7 @@ class StatTest(TestBase):
         return True
 
     def test_incorrect(self):
-        response = self.client.get(self.incorrect_url)
+        response = CLIENT.get(SERVER_ADDRESS + "asdasdasd" + "/stats/")
         assert response.status_code == 404, "Response code should be 404"
         print("All shortcode redirect related tests passed regarding incorrect cases!")
         return True
-
-
-def main():
-    # This is the main function to run the tests
-    # Variables
-    code_chars = (
-            string.ascii_uppercase
-            + string.ascii_lowercase
-            + string.digits + "_")
-
-    test_shortcode = random.sample(code_chars, 6)
-
-    client = RequestsClient()
-
-    server_address = "http://127.0.0.1:8000/"
-    url_shortcode = {"url": "https://www.example.com/", "shortcode": "".join(test_shortcode)}
-
-    shorten_test = ShortenTest(client, server_address, url_shortcode)
-    shorten_test.test_all()
-
-    shortcode_test = ShortCodeTest(client, server_address, url_shortcode)
-    shortcode_test.test_all()
-
-    stat_test = StatTest(client, server_address, url_shortcode)
-    stat_test.test_all()
-
-    print("All tests passed in all cases, the API is working correctly!")
-    return True
-
-
-if __name__ == '__main__':
-    main()
